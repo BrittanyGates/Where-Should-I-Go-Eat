ed62b48bafa8032049d055b10c4543728bb568ce
Brittany Gates <bcgates82@gmail.com>
2025-04-24 18:17:17 -0500

    Uploaded files
    Uploaded files for the branch.

---
+++ static/files/index.js
@@ -0,0 +1,19 @@
+const chainRestaurant = document.querySelector("#chainRestaurantButton");
+const localRestaurantButton = document.querySelector("#localRestaurantButton");
+const resetButton = document.querySelector("#resetButton");
+
+chainRestaurant.addEventListener("click", () => {
+    fetch("/")
+    .then(response => {
+        return response.json()
+    })
+
+});
+
+localRestaurantButton.addEventListener("click", () => {
+    window.open("https://www.google.com/maps/search/?api=1&query=restaurants", "_blank");
+});
+
+resetButton.addEventListener("click", () => {
+    location.href = location.href;
+});
